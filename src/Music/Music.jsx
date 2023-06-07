335f46c07c5b946fc36d70e05d15ac548b69d604
santiago <124795721+santttimunoz@users.noreply.github.com>
2023-06-07 01:43:05 -0500

    crear el refreshToken y pintar las canciones

--- src/Music/Music.jsx
+++ src/Music/Music.jsx
@@ -6,7 +6,8 @@ export function Music() {
     const [cargando, setCargando] = useState(true)
     useEffect(function () {
 
-        musicService().then(function (respuesta) {            
+        musicService().then(function (respuesta) {
+            console.log(respuesta)
             setCanciones(respuesta.tracks)
             setCargando(false)
         })
@@ -17,23 +18,40 @@ export function Music() {
         //gif o loading
         return (
             <>
-            <h1>hola</h1>
+                <h1>hola</h1>
             </>
         )
-        
-    }else{
+
+    } else {
         return (
             <>
-                {
-                    canciones.map(function (cancion) {
-                        return (
-                            
-                                <audio controls src={cancion.preview_url}></audio>
-                                
-                            
-                        )
-                    })
-                }
+                <div className="row">
+                    {
+                        canciones.map(function (cancion) {
+
+
+                            return (
+
+
+
+                                <div className="col-md-2 my-2">
+                                    <div className="card" >
+                                        <img className="card" src={cancion.album.images[0].url} alt="Card image cap" />
+                                        <div className="card-body">
+                                            <h5 className="card-title h6">{cancion.name}</h5>
+                                            <audio controls src={cancion.preview_url} className="w-100"></audio>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+
+                            )
+
+                        })
+                    }
+                </div>
             </>
         )
     }
--- src/services/musicService.js
+++ src/services/musicService.js
@@ -5,16 +5,28 @@ export async function musicService(){
     //definir URL y EP(endPoint = nombre unico) del servicio
     const IDARTIST = "776Uo845nYHJpNaStv1Ds4"
     const URL = `https://api.spotify.com/v1/artists/${IDARTIST}/top-tracks?market=US`
+    const URI ="https://accounts.spotify.com/api/token"
 
-    const TOKEN = "Bearer BQBU5J8ccroPZ2PjyAxac2gWaaO-4kwGJ8dwQo9dNqRyXmAc--H0KJWUAm7H3R2_msZtOoXxpSFTYaRsPRMTTBjAm8flrJzEHjnWG_i1e6SMCPO0BvQ"
+    //este token espira y es obsoleto
+    const TOKEN = "Bearer BQB08dkrmn9qNV2hJAA1CVgV9OU3MZZ30CtoQf0W4-nCeYk35SPBkjaM5tZR7wHjutRAQMr9R4sbTZOIS7ro1v96jV50SWcj1Uc6g7kTEuzREYG6BJA"
 
-    //configurar la peticion
+    
+    let refreshToken = {
+        method: "POST",
+        body:{
+            grant_type: "client_credentials",
+            client_id: "5938fd7d0e5f4d889ba4db749d5780ad",
+            client_secret: "63d8eca3cf88416ca881b4f92588b05b"
+        }
+    }
+//configurar la peticion
     let peticion = {
         method: "GET",
         headers: {
-            "Authorization":TOKEN
+            "Authorization":refreshToken
         }
     }
+
 
     //utilizar la promesa fetchc para ir al back y consumir el API
     let repuesta = await fetch(URL,peticion)
